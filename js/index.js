b055902f51b8841a2e2e9da627466521bfa16811
Sam <sbahlibi@mail.sfsu.edu>
2020-10-28 01:26:56 -0700

    Added the signup modal

--- js/index.js
+++ js/index.js
@@ -6,16 +6,29 @@ var loginModal = document.querySelector("[class='navbar-login-modal']");
 var loginTrigger = document.querySelector("[class='navbar-login']");
 var loginCloseButton = document.querySelector("[class='login-close-button']");
 
-function toggleModal() {
-    loginModal.classList.toggle("show-modal");
+var signupModal = document.querySelector("[class='navbar-signup-modal']");
+var signupTrigger = document.querySelector("[class='navbar-signup']");
+var signupCloseButton = document.querySelector("[class='signup-close-button']");
+
+function toggleLoginModal() {
+    loginModal.classList.toggle("show-login-modal");
 }
 
-function windowOnClick(event) {
+function toggleSignupModal() {
+    signupModal.classList.toggle("show-signup-modal");
+}
+
+function loginWindowOnClick(event) {
     if (event.target === loginModal) {
-        toggleModal();
+        toggleLoginModal();
+    }
+    if (event.target === signupModal) {
+        toggleSignupModal();
     }
 }
 
-loginTrigger.addEventListener("click", toggleModal);
-loginCloseButton.addEventListener("click", toggleModal);
-window.addEventListener("click", windowOnClick);
+loginTrigger.addEventListener("click", toggleLoginModal);
+loginCloseButton.addEventListener("click", toggleLoginModal);
+signupTrigger.addEventListener("click", toggleSignupModal);
+signupCloseButton.addEventListener("click", toggleSignupModal);
+window.addEventListener("click", loginWindowOnClick);
