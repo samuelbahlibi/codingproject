2eb06422547022b43472958b600335b4a31ccfea
Sam <sbahlibi@mail.sfsu.edu>
2020-10-28 00:18:04 -0700

    Added a login modal

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
 const onWindowLoaded = () => {
     console.log('window loaded');
 }
+
+var loginModal = document.querySelector("[class='navbar-login-modal']");
+var loginTrigger = document.querySelector("[class='navbar-login']");
+var loginCloseButton = document.querySelector("[class='login-close-button']");
+
+function toggleModal() {
+    loginModal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === loginModal) {
+        toggleModal();
+    }
+}
+
+loginTrigger.addEventListener("click", toggleModal);
+loginCloseButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
